e75306a965c444b747deff996cf96ef75cfa86f1
Ayan <ayan2572006@gmail.com>
2024-03-07 11:07:58 +0530

    done authenticated user & authorized role

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import asyncHandler from "./asynchandler.js";
+
+const authenticate = asyncHandler(async(req,res,next)=>{
+    let token;
+    //Read JWT from the 'jwt' cookie
+    token=req.cookie.jwt
+    if(token){
+        try {
+            const decoded = jwt.verify(token,process.env.JWT_SECRET)
+            req.user = await User.findById(decoded.userId).select('-password')
+            next()
+        } catch (error) {
+            res.status(401)
+            throw new Error("Not authorized, login faild")
+        }
+        
+    }
+    else{
+        res.status(401)
+        throw new Error("Not authorized, login faild")
+
+    }
+})
+
+//check for the admin
+const authorizedAdmin = (req,res,next)=>{
+    if(req.user && req.user.isAdmin){
+        next()
+    }else{
+        res.status(401).send("Not authorized as an admin")
+    }
+}
+export {authenticate,authorizedAdmin}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createUser, loginUser,logoutCurrentUser } from "../controllers/userController.js";
-
+import { authenticate,authorizedAdmin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 router.route("/").post(createUser);
 router.post("/auth", loginUser);
