506e0a07ab43afd4dbba7c7144ccc2e2d1fa84ec
Ayan <ayan2572006@gmail.com>
2024-03-08 12:20:53 +0530

    userdelete fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -109,17 +109,17 @@ const updateCurrentUserProfile = asyncHandler(async (req, res) => {
 });
 
 const deleteUserById = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);  
+  const user = await User.findById(req.params.id);
   if (user) {
     if (user.isAdmin) {
       res.status(400);
       throw new Error("Cannot delete admin user");
     }
-    await user.deleteOne({ _id: user._id });
-    res.json({message:"user removed"})
-  }else{
-    res.status(400);
-    throw new Error("Cannot delete admin user");
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: "user removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
 });
 export {
