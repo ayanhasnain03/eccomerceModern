53e379f293ab88368d68ce9df64625cae5f3d05a
Ayan <ayan2572006@gmail.com>
2024-03-09 20:44:12 +0530

    category api done

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,40 +1,79 @@
 import asyncHandler from "../middlewares/asynchandler.js";
 import Category from "../models/categoryModel.js";
 
+
 const createCategory = asyncHandler(async (req, res) => {
-    try {
-      const { name } = req.body;
-  
-      if (!name) {
-        return res.json({ error: "Name is required" });
-      }
-  
-      const existingCategory = await Category.findOne({ name });
-  
-      if (existingCategory) {
-        return res.json({ error: "Already exists" });
-      }
-  
-      const category = await new Category({ name }).save();
-      res.json(category);
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json(error);
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.json({ error: "Name is required" });
     }
-  });
 
+    const existingCategory = await Category.findOne({ name });
 
-  
-  const updateCategory = asyncHandler(async(req,res)=>{
-    const {name}=req.body;
-    const {categoryId}=req.params;
-const category = await Category.findOne({_id:categoryId})
-if(!category){
+    if (existingCategory) {
+      return res.json({ error: "Already exists" });
+    }
+
+    const category = await new Category({ name }).save();
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  const { categoryId } = req.params;
+  const category = await Category.findOne({ _id: categoryId });
+  if (!category) {
     return res.status(404).json({ error: "Category not found" });
-}
-category.name=name 
+  }
+  category.name = name;
 
-const updatedCategory = await category.save()
-res.json(updatedCategory);
-  })
-  export {createCategory,updateCategory}
+  const updatedCategory = await category.save();
+  res.json(updatedCategory);
+});
+
+
+
+const removeCategory = asyncHandler(async (req, res) => {
+  try {
+    const removed = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json(removed);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const listCategory = asyncHandler(async (req, res) => {
+  try {
+ const all = await Category.find({})
+ res.json(all)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+const readCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findOne({ _id: req.params.id });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+
+
+
+
+
+
+export { createCategory, updateCategory,removeCategory,listCategory, readCategory};
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authenticate,authorizedAdmin } from "../middlewares/authMiddleware.js";
-import {createCategory,updateCategory} from "../controllers/categoryController.js"
+import {createCategory,updateCategory,removeCategory,listCategory,readCategory} from "../controllers/categoryController.js"
 const router = express.Router()
 router.route("/").post(authenticate,createCategory);
 router.route("/:categoryId").put(authenticate, authorizedAdmin, updateCategory);
-
+router.route("/:categoryId").delete(authenticate, authorizedAdmin, removeCategory);
+router.route("/categories").get(listCategory)
+router.route("/:id").get(readCategory)
 
 export default router;
