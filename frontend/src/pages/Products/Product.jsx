99f64a70793ac9a11ff1697ac3c321b8f365240d
Ayan <ayan2572006@gmail.com>
2024-03-14 03:28:53 +0530

    add to fav and save local storage done

--- frontend/src/pages/Products/Product.jsx
+++ frontend/src/pages/Products/Product.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import HeartIcon from "./HeartIcon";
 
 
 const Product = ({ product }) => {
@@ -10,6 +11,7 @@ const Product = ({ product }) => {
           alt={product.name}
           className="w-[30rem] rounded"
         />
+        <HeartIcon product={product}/>
       </div>
 
       <div className="p-4">
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "./features/auth/authSlice"
+import favoritesReducer from "../redux/features/favorites/favoriteSlice";
+import { getFavoritesFromLocalStorage } from "../utils/localStorage";
+
+const initialFavorites=getFavoritesFromLocalStorage() || []
+
+
 const store = configureStore({
     reducer:{
         [apiSlice.reducerPath]:apiSlice.reducer,
         auth:authReducer,
+        favorites: favoritesReducer,
+    },
+    preloadedState:{
+      favorites:initialFavorites
     },
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
