e93ae149541c5c739231ed41c925d4c5d17286dc
Ayan <ayan2572006@gmail.com>
2024-03-14 03:37:01 +0530

    fav count

--- frontend/src/pages/auth/Navigation.jsx
+++ frontend/src/pages/auth/Navigation.jsx
@@ -13,6 +13,7 @@ import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
+import FavoritesCount from "../Products/FavoritesCount";
 
 const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -76,6 +77,7 @@ const Navigation = () => {
             <span className="hidden nav-item-name mt-[3rem]">
               Favorites
             </span>{" "}
+            <FavoritesCount/>
           </div>
         </Link>
       </div>
