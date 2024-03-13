753ba0a692f4a77c2df657cc3b914387b0a77aee
Ayan <ayan2572006@gmail.com>
2024-03-14 02:43:59 +0530

    Home Done

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,9 @@
-import { Link, useParams } from "react-router-dom";
-import { useGetProductsQuery } from "./redux/api/productApiSlice";
-import Loader from "./components/Loader";
-import Header from "./components/Header";
-import Message from "./components/Message";
-import Product from "./pages/Products/Product";
+ import { Link, useParams } from "react-router-dom";
+ import { useGetProductsQuery } from "./redux/api/productApiSlice";
+ import Loader from "./components/Loader";
+ import Header from "./components/Header";
+ import Message from "./components/Message";
+ import Product from "./pages/Products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
