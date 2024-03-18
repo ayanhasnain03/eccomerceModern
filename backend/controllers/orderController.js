1088a1fe3fb3400e44302c639a34ffd46f9275d8
Ayan <ayan2572006@gmail.com>
2024-03-19 01:50:46 +0530

    order Api Done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -156,6 +156,49 @@ const findOrderById = async (req, res) => {
   }
 };
 
+const markOrderAsPaid = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address,
+      };
+
+      const updateOrder = await order.save();
+      res.status(200).json(updateOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const markOrderAsDelivered = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 export {
   createOrder,
   getAllOrders,
@@ -164,4 +207,6 @@ export {
   calculateTotalSales,
   calcualteTotalSalesByDate,
   findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,7 +6,7 @@ import {
   authenticate,
   authorizedAdmin,
 } from "../middlewares/authMiddleware.js";
-import { createOrder, getAllOrders,getUserOrders,countTotalOrders,calculateTotalSales,calcualteTotalSalesByDate, findOrderById } from "../controllers/orderController.js";
+import { createOrder, getAllOrders,getUserOrders,countTotalOrders,calculateTotalSales,calcualteTotalSalesByDate, findOrderById, markOrderAsPaid, markOrderAsDelivered, } from "../controllers/orderController.js";
 router
   .route("/")
   .post(authenticate, createOrder)
@@ -16,4 +16,9 @@ router
   router.route('/total-sales').get(calculateTotalSales);
   router.route('/total-sales-by-date').get(calcualteTotalSalesByDate);
   router.route("/:id").get(authenticate, findOrderById);
+  router.route('/:id/pay').put(authenticate,markOrderAsPaid);
+  router
+  .route("/:id/deliver")
+  .put(authenticate, authorizedAdmin, markOrderAsDelivered);
+
 export default router;
